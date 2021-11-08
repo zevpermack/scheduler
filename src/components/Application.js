1d6c5bc6f1158b2aa338a75854664bc5219481f4
rewpt <zpermack@gmail.com>
2021-11-08 12:36:37 -0500

    add componenets to sidebar

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,6 +6,18 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu"></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
