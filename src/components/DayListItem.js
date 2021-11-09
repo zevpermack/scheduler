a8ce2cfb996074f87171c08740a9131489635415
rewpt <zpermack@gmail.com>
2021-11-09 16:06:40 -0500

    complete test story for button and create DayListItem component with styling

---
+++ src/components/DayListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'components/DayListItem.scss'
+var classNames = require('classnames');
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    'day-list__item--selected' : props.selected,
+    'day-list__item--full' : (props.spots === 0),
+  })
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+}
