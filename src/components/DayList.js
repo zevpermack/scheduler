f7c7be3df96dac2a3afce85efc105fb9a32af28a
rewpt <zpermack@gmail.com>
2021-11-12 15:33:57 -0500

    create DayList componenet including props passed

---
+++ src/components/DayList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const {onChange} = props
+  const dayList = props.days.map(day => {
+
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={() => onChange(day.name)}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {dayList}
+    </ul>
+  );
+}
