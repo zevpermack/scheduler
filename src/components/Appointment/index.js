0957708f21d4f6175b8ca8f9c3cc374492fbac98
rewpt <zpermack@gmail.com>
2021-11-12 15:33:18 -0500

    complete appointment object including conditional show component vs empty

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import 'components/Appointment/styles.scss'
+import Header from "components/Appointment/Header"
+import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty"
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}  /> : <Empty />}
+    </article>
+  );
+}
