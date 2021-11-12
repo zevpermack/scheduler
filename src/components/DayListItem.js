69d64ecb0ac041a7d8d9a42d4543a51fc49e857a
rewpt <zpermack@gmail.com>
2021-11-12 15:35:27 -0500

    DayListItem component created and working with all passed props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,17 @@ var classNames = require('classnames');
 
 export default function DayListItem(props) {
 
+  const formatSpots = (spots) => {
+    switch (spots) {
+      case 1:
+        return '1 spot remaining';
+      case 0: 
+        return 'no spots remaining';
+      default:
+        return `${spots} spots remaining`;
+    }
+  }
+
   const dayClass = classNames("day-list__item", {
     'day-list__item--selected' : props.selected,
     'day-list__item--full' : (props.spots === 0),
@@ -12,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
