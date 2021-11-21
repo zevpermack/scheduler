46a5aa4f340e8016a3fc35f1ad4e1c2caaada53b
rewpt <zpermack@gmail.com>
2021-11-20 21:03:32 -0500

    add create mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,27 @@ import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
 import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 export default function Appointment(props) {
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}  /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()}  />}
     </article>
   );
 }
