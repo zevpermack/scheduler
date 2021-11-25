3ab1e040d51a16b9ea5ee8f90ce0452245496a5e
rewpt <zpermack@gmail.com>
2021-11-25 11:44:09 -0500

    add conditional logic for rendering name to avoid error if name not present

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -7,8 +7,8 @@ export default function Show(props) {
   <section className="appointment__card-left">
     <h2 className="text--regular">{student}</h2>
     <section className="interviewer">
-      <h4 className="text--light">{interviewer.name}</h4>
-      <h3 className="text--regular">{student}</h3>
+      <h4 className="text--light">Interviewer</h4>
+      <h3 className="text--regular">{interviewer && interviewer.name}</h3>
     </section>
   </section>
   <section className="appointment__card-right">
