8c32ed79f9f5796d807a832629f10208098c926d
rewpt <zpermack@gmail.com>
2021-11-25 11:45:32 -0500

    put interviewer list in ul to render with correct styling

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -19,8 +19,9 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
       {interviewerList}
+      </ul>
     </section>
   );
 }
