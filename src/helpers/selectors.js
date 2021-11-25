76a9a14c585b226a958eaca65900a283be29ef15
rewpt <zpermack@gmail.com>
2021-11-25 11:42:24 -0500

    clean up logic in all selectors and correct logical for getInterviewersForADay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,50 +8,29 @@ export function getAppointmentsForDay(state, day) {
       return appObj;
     }
   }
-  return [];
+  return appObj;
 }
 
 export function getInterview(state, interview) {
-  const retObj = {};
   if (interview) {
-    retObj.student = interview.student;
-    retObj.interviewer = {};
-    retObj.interviewer = state.interviewers[interview.interviewer]
-    return retObj
+    return {
+      student : interview.student, 
+      interviewer : state.interviewers[interview.interviewer]
+    }
   }
   return null;
 }
 
-// This follows the app but doesn't make any sense with the testing state
 
-// export function getInterviewersForDay(state, day) {
-//   const intArr = [];
-//   for (const d of state.days) {
-//     if (day === d.name) {
-//       for(let app of d.appointments){
-//         if (state.appointments[app].interview) {
-//           //If there is an interview for that appointment grab the interviewer id
-//           const interviewerId = state.appointments[app].interview.interviewer
-//           intArr.push(state.interviewers[interviewerId])
-//         }
-//       }
-//     }   
-//   }
-//   if (intArr.length > 0) return intArr;
-//   return [];
-// }
-
-
-// This makes way more sense with app, but doesn't follow the test state
-export function getInterviewersForDay(state, day) {
-  const intArr = [];
-  for (const d of state.days) {
-    if (day === d.name) {
-      for (const int of d.interviewers) {
-        intArr.push(state.interviewers[int]);
-      }
-    }   
-  }
-  if (intArr.length > 0) return intArr;
-  return [];
-}
+ export function getInterviewersForDay(state, day) {
+   const intArr = [];
+   for (const d of state.days) {
+     if (day === d.name) {
+       for (const interviewerId of d.interviewers) {
+         intArr.push(state.interviewers[interviewerId]);
+       }
+       return intArr
+     }   
+   }
+   return intArr;
+ }
