07df5b9bc9df3bcaf60e64dba616ffc7af574259
rewpt <zpermack@gmail.com>
2021-11-29 23:14:00 -0500

    update formatting

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
 import { setSpots } from "helpers/selectors";
 
 
