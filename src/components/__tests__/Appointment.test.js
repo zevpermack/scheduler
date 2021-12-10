4983220feb5d0c96e761de3ae7123f3e942d947d
rewpt <zpermack@gmail.com>
2021-12-10 12:28:59 -0500

    add test for rendering appointment component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment/index";
+
+afterEach(cleanup);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+});
